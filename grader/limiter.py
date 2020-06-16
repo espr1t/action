b6b3aecf42d0be3b7c94b2c0885498bbc2ac3b06
Alexander Georgiev <espr1t.net@gmail.com>
2020-06-16 21:48:55 +0300

    Added rate limiter for Intel RDT.

---
+++ grader/limiter.py
@@ -0,0 +1,40 @@
+import os
+import config
+
+
+NUM_CPUS = config.MAX_PARALLEL_EXECUTORS
+NUM_CACHE_WAYS = 11
+MAX_MEMORY_BANDWIDTH = 100
+
+
+def set_rdt(path, cpu, mem, llc):
+    print("  >> configuring '{}'...".format(path))
+    if not os.path.exists(path):
+        os.system("sudo mkdir {path}".format(path=path))
+    os.system("echo '{cpu}' | sudo tee {path}/cpus_list".format(cpu=cpu, path=path))
+    os.system("echo 'MB:0={mem}' | sudo tee {path}/schemata".format(mem=mem, path=path))
+    os.system("echo 'L3:0={llc}' | sudo tee {path}/schemata".format(llc=format(llc, 'x'), path=path))
+
+
+def set_limits():
+    # Mount the Intel RDT Linux control filesystem
+    if not os.path.ismount("/sys/fs/resctrl"):
+        print("Mounting resctrl filesystem...")
+        os.system("sudo mount -t resctrl resctrl /sys/fs/resctrl")
+    else:
+        print("Filesystem resctrl already mounted.")
+
+    # Calculate and set the default values (for COS0)
+    cache_bits = NUM_CACHE_WAYS // NUM_CPUS
+    cache_mask = (1 << cache_bits) - 1
+    print("Using {}/{} LLC ways...".format(cache_bits, NUM_CACHE_WAYS))
+
+    print("Starting configuration...")
+    for i in range(NUM_CPUS):
+        set_rdt("/sys/fs/resctrl/COS{}".format(i + 1), i, 10, cache_mask)
+        cache_mask <<= cache_bits
+    print("Done!")
+
+
+if __name__ == "__main__":
+    set_limits()
