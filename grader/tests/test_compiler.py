cf159263aaa27a672a6df9f43417006e5332d1ec
Alexander Georgiev <espr1t.net@gmail.com>
2020-09-06 01:11:37 +0300

    Made time limit for compilation tests more lenient.

--- grader/tests/test_compiler.py
+++ grader/tests/test_compiler.py
@@ -120,7 +120,7 @@ class TestCompiler(TestCase):
         message = Compiler.compile(config.LANGUAGE_JAVA, path_source, path_executable)
         self.assertEqual("", message, "The Java compilation expected to pass, but failed.")
         self.assertTrue(os.path.exists(path_executable))
-        self.assertLess(perf_counter() - start_time, 3.0)  # 3 seconds should be enough for the compilation
+        self.assertLess(perf_counter() - start_time, 5.0)  # 5 seconds should be enough for the compilation
 
     def test_java_unsuccessful_compilation(self):
         path_source = os.path.join(self.PATH_FIXTURES, "java/HelloWorldCE.java")
